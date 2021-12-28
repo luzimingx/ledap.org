0226abc104c6258be87b953c6addc3cfb9dea70d
luziming <luzimingx@gmail.com>
2021-12-28 14:03:48 -0400

    迁移文档地址

--- script/deploy.sh
+++ script/deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/ethercap/ledap.org.git master:gh-pages
+git push -f https://github.com/luzimingx/ledap.org.git master:gh-pages
 
 cd -
